e9495b9a75f70c846ca80efbed3952bc0d34a5ef
kubanaj <kubanaj@gmail.com>
2023-04-01 00:57:17 +0200

    added Canvas to Display class
    Canvas draw graphics into frame

--- src/main/java/fel/cvut/cz/display/Display.java
+++ src/main/java/fel/cvut/cz/display/Display.java
@@ -1,10 +1,12 @@
 package fel.cvut.cz.display;
 
 import javax.swing.*;
+import java.awt.*;
 
 /** Class that makes frame to display game */
 public class Display {
     private JFrame frame;
+    private Canvas canvas; //draw graphics in JFrame
     private final String title;
     private final int width;
     private final int height;
@@ -16,7 +18,7 @@ public class Display {
 
         createDisplay();
     }
-    /** Initialize JFrame */
+    /** Initialize JFrame + Canvas */
     private void createDisplay(){
         this.frame = new JFrame(this.title);
         this.frame.setSize(width,height);
@@ -24,5 +26,13 @@ public class Display {
         this.frame.setResizable(false); //User cannot resize window
         this.frame.setLocationRelativeTo(null); //Window appears in the center of the screen
         this.frame.setVisible(true); //Otherwise we won't see anything
+
+        this.canvas = new Canvas();
+        this.canvas.setPreferredSize(new Dimension(this.width, this.height)); //sets size of my canvas.
+        this.canvas.setMaximumSize(new Dimension(this.width, this.height)); //so that canvas stays the same
+        this.canvas.setMinimumSize(new Dimension(this.width, this.height)); //width and height always
+
+        this.frame.add(this.canvas);
+        frame.pack(); //resize window a little, so we can see the canvas fully
     }
 }
